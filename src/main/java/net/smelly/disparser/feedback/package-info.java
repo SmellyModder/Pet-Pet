cce108b33abc5baf453cc2a33079f3e4ad5b294b
SmellyModder <smellysocks543@gmail.com>
2020-09-23 15:30:38 -0400

    Use Disparser in build.gradle instead of inside project

--- src/main/java/net/smelly/disparser/feedback/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This package contains all the built in classes and interfaces that can be used for sending command processing feedback in Disparser.
- * Most classes and interfaces in this package are used internally in Disparser.
- *
- * @author Luke Tonon
- * @version 1.1.0
- */
-package net.smelly.disparser.feedback;
--- src/main/java/net/smelly/disparser/util/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains some utility classes for commands.
- *
- * @author Luke Tonon
- * @version 1.0.0
- */
-package net.smelly.disparser.util;
