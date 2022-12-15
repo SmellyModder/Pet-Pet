4e1010a23a82a60ca50382ebc07ebdfbb1845d73
SmellyModder <41268323+SmellyModder@users.noreply.github.com>
2022-12-15 13:11:03 -0500

    Ported to slash commands, closes #5

--- src/main/java/net/smelly/petpet/PetPet.java
+++ src/main/java/net/smelly/petpet/PetPet.java
@@ -1,52 +1,90 @@
 package net.smelly.petpet;
 
-import net.smelly.disparser.CommandHandler;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.smelly.petpet.commands.PPCommands;
-
-import javax.security.auth.login.LoginException;
+import net.smelly.petpet.commands.PetPetCommand;
 
 /**
  * @author Luke Tonon
  */
 public final class PetPet {
+	private static String gifDirectory;
+
 	/**
 	 * @param args:
 	 *            0 - bot token
-	 *            1 - prefix storage directory
-	 *            2 - gif directory
-	 * @throws LoginException
+	 *            1 - gif directory
 	 */
-	public static void main(String[] args) throws LoginException {
-		JDABuilder botBuilder = JDABuilder.create(args[0], GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS & ~GatewayIntent.getRaw(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING, GatewayIntent.DIRECT_MESSAGE_TYPING)));
+	public static void main(String[] args) {
+		JDABuilder botBuilder = JDABuilder.create(args[0], GatewayIntent.getIntents(GatewayIntent.DEFAULT));
 		botBuilder.setStatus(OnlineStatus.ONLINE);
-		botBuilder.setActivity(Activity.of(Activity.ActivityType.DEFAULT, "Petting Images!"));
+		botBuilder.setActivity(Activity.of(Activity.ActivityType.LISTENING, "/petpet"));
 		botBuilder.addEventListeners(new PetPetCommandHandler());
-		botBuilder.build();
-		ServerDataManager.INSTANCE.init(args[1], args[2]);
+		botBuilder.build()
+				.updateCommands().addCommands(
+						Commands.slash("petpet", "Generates a GIF of a hand petting an image or user.")
+								.addSubcommands(
+										addBasePetPetOptions(
+												new SubcommandData("user", "Generates a GIF of a hand petting a user.")
+														.addOption(OptionType.USER, "user", "The user to pet")
+										),
+										addBasePetPetOptions(
+												new SubcommandData("image", "Generates a GIF of a hand petting an image.")
+														.addOption(OptionType.ATTACHMENT, "image", "The image to pet", true)
+										),
+										addBasePetPetOptions(
+												new SubcommandData("url", "Generates a GIF of a hand petting an image from a url.")
+														.addOption(OptionType.STRING, "url", "The URL of the image to pet", true)
+										)
+								)
+				).queue();
+		gifDirectory = args[1];
 	}
 
-	static class PetPetCommandHandler extends CommandHandler {
+	public static String getGifDirectory() {
+		return gifDirectory;
+	}
 
-		private PetPetCommandHandler() {
-			super((guild) -> ServerDataManager.INSTANCE.getPrefix(guild));
-			this.registerCommands(PPCommands.class);
-		}
+	private static SubcommandData addBasePetPetOptions(SubcommandData slashCommandData) {
+		return slashCommandData.addOption(OptionType.INTEGER, "fps", "The frames per second for the gif")
+				.addOption(OptionType.NUMBER, "scale", "The scale of the image getting pet for the gif");
+	}
+
+	static class PetPetCommandHandler extends ListenerAdapter {
+
+		private PetPetCommandHandler() {}
 
 		@Override
-		public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-			this.registerCommand(PPCommands.createInfoCommand(event.getGuild()));
-			super.onGuildMessageReceived(event);
-		}
-
-		@Override
-		public void onGuildJoin(GuildJoinEvent event) {
-			ServerDataManager.INSTANCE.updateBotNickname(event.getGuild(), null);
+		public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+			if (event.getName().equals("petpet")) {
+				OptionMapping fpsOption = event.getOption("fps");
+				int fps = fpsOption != null ? fpsOption.getAsInt() : 15;
+				OptionMapping scaleOption = event.getOption("scale");
+				float scale = scaleOption != null ? (float) scaleOption.getAsDouble() : 1.0F;
+				String subcommand = event.getSubcommandName();
+				if ("image".equals(subcommand)) {
+					OptionMapping imageOption = event.getOption("image");
+					if (imageOption != null) {
+						PetPetCommand.processCommand(event, null, imageOption.getAsAttachment().getUrl(), fps, scale);
+					}
+				} else if ("url".equals(subcommand)) {
+					OptionMapping urlOption = event.getOption("url");
+					if (urlOption != null) {
+						PetPetCommand.processCommand(event, null, urlOption.getAsString(), fps, scale);
+					}
+				} else {
+					OptionMapping userOption = event.getOption("user");
+					PetPetCommand.processCommand(event, userOption != null ? userOption.getAsUser() : event.getUser(), null, fps, scale);
+				}
+			}
 		}
 
 	}
--- src/main/java/net/smelly/petpet/commands/PetPetCommand.java
+++ src/main/java/net/smelly/petpet/commands/PetPetCommand.java
@@ -2,20 +2,12 @@ package net.smelly.petpet.commands;
 
 import com.squareup.gifencoder.*;
 import com.twelvemonkeys.image.ResampleOp;
-import net.smelly.disparser.Command;
-import net.smelly.disparser.CommandContext;
-import net.smelly.disparser.arguments.EitherArgument;
-import net.smelly.disparser.arguments.java.FloatArgument;
-import net.smelly.disparser.arguments.java.IntegerArgument;
-import net.smelly.disparser.arguments.java.URLArgument;
-import net.smelly.disparser.arguments.jda.UserArgument;
-import net.smelly.disparser.feedback.exceptions.BiDynamicCommandExceptionCreator;
-import net.smelly.disparser.feedback.exceptions.CommandSyntaxException;
-import net.smelly.disparser.feedback.exceptions.SimpleCommandExceptionCreator;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
-import net.smelly.petpet.ServerDataManager;
+import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.smelly.petpet.PetPet;
 import net.smelly.petpet.ThreadedImageBrightener;
+import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.*;
 import java.awt.*;
@@ -23,127 +15,86 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.lang.reflect.Field;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 
 /**
  * @author Luke Tonon
  */
-public final class PetPetCommand extends Command {
+public final class PetPetCommand {
 	private static final int FPS_CAP = 60;
 	private static final int HAND_SIZE = 112;
 	private static final int DEFAULT_X = 12;
 	private static final int DEFAULT_Y = 18;
 	private static final float PET_SIZE = 100.0F;
-	private static final String USER_AGENT = "User-Agent";
 	private static final String BOT_USER_AGENT = "DiscordBot (https://github.com/DV8FromTheWorld/JDA, 4.2.0_204)";
 
-	private static final BiDynamicCommandExceptionCreator<String, Boolean> UNABLE_TO_FETCH_EXCEPTION = BiDynamicCommandExceptionCreator.createInstance((string, bool) -> {
-		return bool ? String.format("Could not retrieve and process image for user %s", string) : String.format("Could not retrieve and process image from url (`%s`)", string);
-	});
-	private static final SimpleCommandExceptionCreator IMAGE_SIZE_TOO_LARGE_EXCEPTION = new SimpleCommandExceptionCreator("Processed image size was too large!");
-	private static final SimpleCommandExceptionCreator IMAGE_READ_EXCEPTION = new SimpleCommandExceptionCreator("An error occurred trying to read the image");
-
-	public PetPetCommand() {
-		super("pet", EitherArgument.of(UserArgument.get(), URLArgument.get()).asOptional(), IntegerArgument.get().asOptional(), FloatArgument.getMin(0.0F).asOptional());
-	}
-
-	@Override
-	public void processCommand(CommandContext context) throws Exception {
-		EitherArgument.Either<User, URL> userURLEither = context.getParsedResult(0);
-		int fps = this.getClampedFPS(context.getParsedResultOrElse(1, 15));
-		float scale = context.getParsedResultOrElse(2, 1.0F);
-		if (userURLEither != null) {
-			User user = userURLEither.first;
-			if (user != null) {
-				this.processURL(context, fps, scale, user.getName(), this.getAvatarURL(user), true);
-			} else {
-				URL url = userURLEither.second;
-				String[] split = url.toString().split("/");
-				this.processURL(context, fps, scale, split[split.length - 1], url, false);
-			}
-		} else {
-			List<Message.Attachment> attachments = context.getEvent().getMessage().getAttachments();
-			for (Message.Attachment attachment : attachments) {
-				if (attachment.isImage()) {
-					try {
-						this.processPet(context, fps, scale, attachment.getFileName(), attachment.retrieveInputStream().get());
-					} catch (OutOfMemoryError error) {
-						throw IMAGE_SIZE_TOO_LARGE_EXCEPTION.create();
-					}
-				}
-			}
-		}
-	}
-
-	private int getClampedFPS(int fps) {
-		return fps > FPS_CAP ? FPS_CAP : fps < 0 ? 1 : fps;
-	}
-
-	private void processURL(CommandContext context, int fps, float scale, String name, URL url, boolean user) throws CommandSyntaxException {
+	public static void processCommand(IReplyCallback replyCallback, @Nullable User user, @Nullable String url, int fps, float scale) {
+		fps = fps > FPS_CAP ? FPS_CAP : fps <= 0 ? 1 : fps;
+		if (scale < 0.0F) scale = 0.0F;
+		String name;
+		String contentURL;
+		if (user != null) {
+			name = user.getName();
+			String avatar = user.getAvatarUrl();
+			contentURL = avatar != null ? avatar : user.getDefaultAvatarUrl();
+		} else if (url != null) {
+			String[] split = url.split("/");
+			name = split[split.length - 1];
+			contentURL = url;
+		} else return;
 		try {
-			URLConnection connection = url.openConnection();
-			connection.setRequestProperty(USER_AGENT, BOT_USER_AGENT);
-			this.processPet(context, fps, scale, name, connection.getInputStream());
+			URLConnection connection = new URL(contentURL).openConnection();
+			connection.setRequestProperty("User-Agent", BOT_USER_AGENT);
+			InputStream inputStream = connection.getInputStream();
+			File outputGif = new File(String.format("%1$s/petpet_%2$s.gif", PetPet.getGifDirectory(), name));
+			FileOutputStream outputStream = new FileOutputStream(outputGif);
+			GifEncoder gifEncoder = new GifEncoder(outputStream, 112, 112, 0);
+			ImageOptions options = new ImageOptions()
+					.setTransparencyColor(Color.BLACK.getRGB())
+					.setColorQuantizer(MedianCutQuantizer.INSTANCE)
+					.setDitherer(FloydSteinbergDitherer.INSTANCE)
+					.setDisposalMethod(DisposalMethod.DO_NOT_DISPOSE);
+			Field delayCentiSeconds = options.getClass().getDeclaredField("delayCentiSeconds");
+			delayCentiSeconds.setAccessible(true);
+			delayCentiSeconds.set(options, Math.round(100.0F / fps));
+			try {
+				BufferedImage bufferedImage = ImageIO.read(new File(PetPet.getGifDirectory() + "/petpet.png"));
+				BufferedImage image = ImageIO.read(inputStream);
+				ThreadedImageBrightener imageDarkener = new ThreadedImageBrightener(image, 0.04F);
+				imageDarkener.run();
+
+				int inputWidth = image.getWidth(null);
+				int inputHeight = image.getHeight(null);
+
+				scale = Math.min(PET_SIZE / (float) inputWidth, PET_SIZE / (float) inputHeight) * scale;
+				int width = (int) ((float) inputWidth * scale);
+				int height = (int) ((float) inputHeight * scale);
+
+				FrameDrawerType[] drawerTypes = FrameDrawerType.values();
+				for (int i = 0; i < drawerTypes.length; i++) {
+					BufferedImage combined = new BufferedImage(HAND_SIZE, HAND_SIZE, BufferedImage.TYPE_INT_ARGB);
+					Graphics combinedGraphics = combined.getGraphics();
+					drawerTypes[i].drawer.draw(combinedGraphics, image, width, height);
+					combinedGraphics.drawImage(bufferedImage.getSubimage(i * 112, 0, 112, 112), 0, 0, null);
+					combinedGraphics.dispose();
+					int[] rgb = combined.getRGB(0, 0, HAND_SIZE, HAND_SIZE, new int[HAND_SIZE * HAND_SIZE], 0, HAND_SIZE);
+					gifEncoder.addImage(rgb, HAND_SIZE, options);
+				}
+
+				gifEncoder.finishEncoding();
+				inputStream.close();
+				replyCallback.replyFiles(FileUpload.fromData(outputGif, outputGif.getName())).queue();
+				outputGif.deleteOnExit();
+			} catch (Exception e) {
+				replyCallback.reply(":x: Command failed, an error occurred trying to read and process the image").queue();
+			}
 		} catch (Throwable e) {
 			if (e instanceof OutOfMemoryError) {
-				throw IMAGE_SIZE_TOO_LARGE_EXCEPTION.create();
+				replyCallback.reply(":x: Command failed, image was too large!").queue();
 			} else {
-				throw UNABLE_TO_FETCH_EXCEPTION.create(user ? name : url.toString(), user);
+				replyCallback.reply(":x: Command failed, could not retrieve image from url (`" + contentURL + "`)").queue();
 			}
-		}
-	}
-
-	private URL getAvatarURL(User user) throws MalformedURLException {
-		String avatar = user.getAvatarUrl();
-		return new URL(avatar != null ? avatar : user.getDefaultAvatarUrl());
-	}
-
-	private void processPet(CommandContext context, int fps, float scale, String name, InputStream stream) throws Exception {
-		String gifDirectory = ServerDataManager.INSTANCE.getGifDirectory();
-		File outputGif = new File(String.format("%1$s/petpet_%2$s.gif", gifDirectory, name));
-		FileOutputStream outputStream = new FileOutputStream(outputGif);
-		GifEncoder gifEncoder = new GifEncoder(outputStream, 112, 112, 0);
-		ImageOptions options = new ImageOptions()
-				.setTransparencyColor(Color.BLACK.getRGB())
-				.setColorQuantizer(MedianCutQuantizer.INSTANCE)
-				.setDitherer(FloydSteinbergDitherer.INSTANCE)
-				.setDisposalMethod(DisposalMethod.DO_NOT_DISPOSE);
-		Field delayCentiSeconds = options.getClass().getDeclaredField("delayCentiSeconds");
-		delayCentiSeconds.setAccessible(true);
-		delayCentiSeconds.set(options, Math.round(100.0F / fps));
-		try {
-			BufferedImage bufferedImage = ImageIO.read(new File(gifDirectory + "/petpet.png"));
-			BufferedImage image = ImageIO.read(stream);
-			ThreadedImageBrightener imageDarkener = new ThreadedImageBrightener(image, 0.04F);
-			imageDarkener.run();
-
-			int inputWidth = image.getWidth(null);
-			int inputHeight = image.getHeight(null);
-
-			scale = Math.min(PET_SIZE / (float) inputWidth, PET_SIZE / (float) inputHeight) * scale;
-			int width = (int) ((float) inputWidth * scale);
-			int height = (int) ((float) inputHeight * scale);
-
-			FrameDrawerType[] drawerTypes = FrameDrawerType.values();
-			for (int i = 0; i < drawerTypes.length; i++) {
-				BufferedImage combined = new BufferedImage(HAND_SIZE, HAND_SIZE, BufferedImage.TYPE_INT_ARGB);
-				Graphics combinedGraphics = combined.getGraphics();
-				drawerTypes[i].drawer.draw(combinedGraphics, image, width, height);
-				combinedGraphics.drawImage(bufferedImage.getSubimage(i * 112, 0, 112, 112), 0, 0, null);
-				combinedGraphics.dispose();
-				int[] rgb = combined.getRGB(0, 0, HAND_SIZE, HAND_SIZE, new int[HAND_SIZE * HAND_SIZE], 0, HAND_SIZE);
-				gifEncoder.addImage(rgb, HAND_SIZE, options);
-			}
-
-			gifEncoder.finishEncoding();
-			stream.close();
-			context.getEvent().getChannel().sendFile(outputGif).queue();
-			outputGif.deleteOnExit();
-		} catch (Exception e) {
-			throw IMAGE_READ_EXCEPTION.create();
 		}
 	}
 
